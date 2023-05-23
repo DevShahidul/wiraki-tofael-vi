9f4a86a6a365a1c2729c484629dd4f2b849f77d5
Shahidul Islam <ishahin56@gmail.com>
2023-05-23 23:02:19 +0600

    Completed the preloader animation

--- js/common-scripts.js
+++ js/common-scripts.js
@@ -2,8 +2,95 @@
 (function($){
 	$(function(){
 
+        // Preloader function scripts
+        $(window).on('beforeunload', function(){
+            $(window).scrollTop(0);
+        });
+
+        function getElementEndPos(el) {
+            var rect = el[0].getBoundingClientRect();
+            return rect.top + rect.height;
+          }
+          function getElementHeight(el){
+            var rect = el[0].getBoundingClientRect();
+            return rect.height;
+          }
+
+        function convertToPercent (elementHeight, scrolledVal){
+            let calculatedValue = elementHeight / 100;
+            return scrolledVal / calculatedValue
+        }
+
+        
+        function reduceInset(percent, el){
+            let inset = 100 - percent;
+            if(inset < 0) {
+                el.find('.filling_item').css({
+                    clipPath: 'inset(0% 0 0)'
+                });
+            }else{
+                el.find('.filling_item').css({
+                    clipPath: 'inset('+inset+'% 0 0)'
+                });
+            }
+          }
+
+          function filling(element, scrollingLayer){
+            let elEndPos = getElementEndPos(element);
+                let elHeight = getElementHeight(element);
+
+              if(scrollingLayer.top <= elEndPos){
+                let elScrolled = elEndPos - scrollingLayer.top;
+                let percent = convertToPercent(elHeight, elScrolled);
+                reduceInset(percent, element)
+                element.find('.filling_item_down').css({
+                    clipPath: 'inset(100% 0 0)'
+                })
+              }
+              if( scrollingLayer.top > elEndPos){  // Reset the clip path to 100%
+                element.find('.filling_item').css({
+                    clipPath: 'inset(100% 0 0)'
+                });
+                element.find('.filling_item_down').css({
+                    clipPath: 'inset(0% 0 0)'
+                })
+              }
+              if(scrollingLayer.top == 0){
+               var timing = setTimeout(function(){
+                   if($(".pre-loader-main").attr("style") !== "display: none;"){
+                       $(window).scrollTop(0);
+                        $(".pre-loader-main").hide();
+                    }
+                    $(".main-wrap").css({
+                        height: "auto"
+                    })
+                    $(".main-wrap").animate({
+                        opacity: 1,
+                    }, 500);
+                }, 700)
+              }
+          }
+
+
+        $(window).on('scroll resize', function() {
+            var scrollingLayer = document.getElementById("black-overlay").getBoundingClientRect();
+        
+            $(".box-card").each(function() {
+                let _this = $(this);
+                filling(_this, scrollingLayer);
+            });
+            $(".innter-text").each(function(){
+                $this = $(this);
+                filling($this, scrollingLayer);
+            });
+          });
+
+        //   End peloader functionality
+
+        
 
         if ($('.venture-item-wrap').length) {
+              
             $('.venture-item-wrap').slick({
                 arrows: false,
                 infinite: true,
@@ -16,10 +103,12 @@
                 centerMode: false,
                 focusOnSelect: false,
 
+            }).on('init', function(event, slick){
+                console.log("initialised")
+                $('.venture-item-wrap').slick()
             });
             $(window).on('resize', function () {
                 $('.venture-item-wrap').slick('resize');
-
             });
         }
         
